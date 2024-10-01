1331dc19ff61ce816c2e9b8ee0db9e5b5b6b0597
Developer@2446 <balu2446madhu@gmail.com>
2024-10-01 17:50:41 +0530

    add login functionality

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,11 +9,43 @@ import {
   import { Label } from "@/components/ui/label"
   import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { Link } from "react-router-dom"
+import { Link,useNavigate } from "react-router-dom"
+
+import { useRef } from "react"
+import { useMutation } from "@tanstack/react-query"
+import { login } from "@/http/api"
 
 
   
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const emailRef=useRef<HTMLInputElement>(null)
+  const passwordRef=useRef<HTMLInputElement>(null)
+
+  const mutation = useMutation({
+    mutationFn: login,
+
+    onSuccess: () => {
+      console.log("login successful");
+      navigate('/dashboard/home');
+      
+    },
+  })
+
+  const handleLoginSubmit=()=>{
+    const email=emailRef.current?.value;
+    const password=passwordRef.current?.value;
+    console.log(email,password);
+    //redirect to dashboard
+    if (!email||!password) {
+      return alert("Please enter email and password")
+      
+    }
+    
+    //make server call.
+    mutation.mutate({email,password})
+  }
+
   return (
     <section className="flex h-screen w-full items-center justify-center">
         <Card className="w-full max-w-sm">
@@ -26,19 +58,19 @@ const LoginPage = () => {
       <CardContent className="grid gap-4">
         <div className="grid gap-2">
           <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
+          <Input ref={emailRef} id="email" type="email" placeholder="m@example.com" required />
         </div>
         <div className="grid gap-2">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" required />
+          <Input  ref={passwordRef} id="password" type="password" required />
         </div>
       </CardContent>
       <CardFooter>
         <div className="w-full">
-          <Button className="w-full">Sign in</Button>
+          <Button  onClick={handleLoginSubmit} className="w-full">Sign in</Button>
         <div className="mt-4 text-center text-sm">
          Dont  have an account?{" "}
-          <Link to={"/register"} className="underline">
+          <Link to={"/auth/register"} className="underline">
             Sign in
           </Link>
         </div></div>
